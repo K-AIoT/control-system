02deb99a57574c7c4a200508797c7606301ef03a
Sarah-Sandell <sarjs@outlook.com>
2023-04-19 14:45:14 +0200

    Added Publish Functionality and Individual Message Callbacks for Subscribers

--- controlSystemMain.py
+++ controlSystemMain.py
@@ -1,8 +1,15 @@
 import mqttModule
 import time
+from random import randrange
 
 mqttClient = mqttModule.makeClient("mqttClient")
+#mqttPublishTopics = ["actuators/servo"]
 
 while True:
+
+    #inputData = randrange(30.0)
+
+    #for topic in mqttPublishTopics:
+        #mqttModule.mqttPublishData(mqttClient, topic, inputData)
 
     time.sleep(1)
--- mqttModule.py
+++ mqttModule.py
@@ -1,14 +1,18 @@
 import paho.mqtt.client as mqtt
 
-mqttBrokerIp = "192.168.50.192"
-mqttClientSubscriptions = ["sensors/temperature","sensors/humidity", "sensors/temperature2"]
+mqttBrokerIp = "172.29.69.158"
+
+mqttClientSubscriptions = ["ping", "sensors/temperature", "sensors/humidity", "sensors/temperature2", "resistors/potmeter"] #Make empty list here, and add function to add subscriptions that can be called from main file.
 
 #Function that creates an MQTT client, defines its callback functions and connects it to the broker via its IP address.
 def makeClient(clientName):
-    client = mqtt.Client(clientName)
+    client = mqtt.Client(client_id=clientName, protocol=mqtt.MQTTv311, transport="tcp")
     client.on_connect = on_connect
     client.on_message = on_message
-    client.message_callback_add("sensors/temperature", tempSensorCallback)
+    client.message_callback_add("sensors/temperature", sensorsTemperatureCallback)
+    client.message_callback_add("sensors/temperature2", sensorsTemperature2Callback)
+    client.message_callback_add("sensors/humidity", sensorsHumidityCallback)
+    client.message_callback_add("resistors/potmeter", resistorsPotmeterCallback)
     client.on_disconnect = on_disconnect
     client.connect(mqttBrokerIp)
     client.loop_start()                                                                                         
@@ -16,12 +20,12 @@ def makeClient(clientName):
 
 #Message callback function, to handle any messages on subscribed topics that don't have a specific callback function.
 def on_message(client, userdata, message):
-    print("Received message: ", str(message.payload.decode("utf-8")))
+    print("Received Message: ", str(message.payload.decode("utf-8")))
 
 #Callback function that executes when client receives CONNACK from broker in response to connect request.
 def on_connect(client, userdata, flags, rc):
     for topic in mqttClientSubscriptions:
-        client.subscribe(topic, 1)
+        client.subscribe(topic, 1)  
 
 #Callback function that executes when client sends disconnect request to broker.
 def on_disconnect(client, userdata, rc):
@@ -29,11 +33,22 @@ def on_disconnect(client, userdata, rc):
     client.loop_stop()
 
 #Message callback function, specifically to handle any messages on topics that match "sensors/temperature" (Including with wildcard use).
-def tempSensorCallback(client, userdata, message):
+def sensorsTemperatureCallback(client, userdata, message):
     print("Received Temp Message: ", str(message.payload.decode("utf-8")))
+    #client.publish("actuators/servo", message.payload)  #Publishes messages received directly to actuators/servo topic
 
+#Message callback function, specifically to handle any messages on topics that match "sensors/temperature2" (Including with wildcard use).
+def sensorsTemperature2Callback(client, userdata, message):
+    print("Received Temp2 Message: ", str(message.payload.decode("utf-8"))) 
 
-def hello():
-    print("Hello World")
+#Message callback function, specifically to handle any messages on topics that match "sensors/humidity" (Including with wildcard use).
+def sensorsHumidityCallback(client, userdata, message):
+    print("Received Humidity Message: ", str(message.payload.decode("utf-8")))  
 
-        
+#Message callback function, specifically to handle any messages on topics that match "sensors/potemeter" (Including with wildcard use).
+def resistorsPotmeterCallback(client, userdata, message):
+    print("Received Potmeter Message: ", str(message.payload.decode("utf-8")))  
+    client.publish("actuators/servo", message.payload)  #Publishes messages received directly to actuators/servo topic
+
+def mqttPublishData(client, topic, data):
+    client.publish(topic, payload=data, qos=1, retain=False)
