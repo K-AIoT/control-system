508fc21f6d9d0550df99083ed94dc6e507c7f064
Sarah-Sandell <sarjs@outlook.com>
2023-04-25 13:05:15 +0200

    Fixing with correct code in file

--- opcuaModule.py
+++ opcuaModule.py
@@ -1,12 +1,39 @@
-import asyncio
-import logging
+    import asyncio
 
-from asyncua import ua, Server
-from asyncua.common.methods import uamethod
+    from asyncua import Client
 
-async def main():
-    #Server setup
-    opcuaServer = Server()
-    await opcuaServer.init()
-    opcuaServer.set_endpoint('opc.tcp://10.64.233.49:4840')
-    opcuaServer.set_server_name("NxTTech IDE Server")
+    opcuaServerUrl = "opc.tcp://10.64.233.49:4840"
+
+    namespaceUri = "http://opcfoundation.org/UA/"
+
+    # async def updateAttributeVal(node, attributeName):
+    #     attributeVal = await asyncio.wait_for(attributeName, None) #Wait for variable_name to change with no timeout (None)
+    #     node.write_value(attributeVal)
+
+    async def opcuaMain():
+
+        print(f"Connecting to {opcuaServerUrl} ...")
+
+        async with Client(url=opcuaServerUrl) as client:
+            # Find the namespace index
+            nsidx = await client.get_namespace_index(namespaceUri)
+            
+            # Get the variable node for read / write
+            var = await client.nodes.root.get_child(
+                ["0:Objects", f"{nsidx}:actuatorInput"]
+            )
+            value = await var.read_value()
+            print(value)
+
+            #new_value = value - 50
+            
+            #await var.write_value(new_value)
+
+            # Calling a method
+            #res = await client.nodes.objects.call_method(f"{nsidx}:ServerMethod", 5)
+            
+
+    if __name__ == "__main__":
+        asyncio.run(opcuaMain())
+
+
