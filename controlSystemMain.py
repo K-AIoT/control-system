f20fb0f3244d88f78f720442b2020e0dfaf86082
Sarah Jane Sandell <sarah.jane.sandell@km.kongsberg.com>
2023-05-02 08:24:52 +0200

    Uploading most recent main and mqttModule that works sending data from robot to main

--- controlSystemMain.py
+++ controlSystemMain.py
@@ -1,17 +1,44 @@
 import mqttModule
+import devices
 import time
+from geometry_msgs.msg import Twist
 from random import randrange
 
 mqttClient = mqttModule.makeClient("mqttClient")
 mqttPublishTopics = ["actuators/servo", "robot/intListToRos", "resistors/potmeter"]
-inputData = [1, 2, 3]
+# inputData = [1, 2, 3]
+
+twist_msg = Twist()
+twist_msg.linear.x = 0.2
+twist_msg.linear.y = 0.0
+twist_msg.linear.z = 0.0
+twist_msg.angular.x = 0.0
+twist_msg.angular.y = 0.0
+twist_msg.angular.z = 0.0
+
 while True:
 
     #inputData['data'] = not inputData['data']
-    for i in range(len(inputData)):
-        inputData[i] += 1
 
-    for topic in mqttPublishTopics:
-        mqttModule.mqttPublishData(mqttClient, topic, inputData)
+    # for i in range(len(inputData)):
+    #     inputData[i] += 1
 
-    time.sleep(1)
+    # for topic in mqttPublishTopics:
+    #     mqttModule.mqttPublishData(mqttClient, topic, inputData)
+
+    # for key in inputData['linear']:
+    #     inputData['linear'][key] = -inputData['linear'][key]
+
+
+    twist_data = {
+        'linear': {'x': twist_msg.linear.x, 'y': twist_msg.linear.y, 'z': twist_msg.linear.z},
+        'angular': {'x': twist_msg.angular.x, 'y': twist_msg.angular.y, 'z': twist_msg.angular.z}}
+    
+    packed_twist_data = msgpack.packb(twist_data)
+
+    mqttClient.publish("robot/cmd_vel", packed_twist_data)
+
+    # mqttModule.mqttPublishData(mqttClient, "robot/toCmd_vel", inputData)
+    
+
+    time.sleep(0.005)
--- mqttModule.py
+++ mqttModule.py
@@ -1,20 +1,21 @@
+# mqttModule.py
+
 import paho.mqtt.client as mqtt
 import msgpack
 import json
 
-mqttBrokerIp = "192.168.137.211"
+mqttBrokerIp = "192.168.50.74" #local home
+# mqttBrokerIp = "172.29.76.226" #local eduroam
+# mqttBrokerIp = "192.168.1.22" #raspberry pi
 
-mqttClientSubscriptions = ["robot/intListToMqtt", "robot/boolToMqtt", "sensors/temperature", "sensors/humidity", "sensors/temperature2", "resistors/potmeter"] #Make empty list here, and add function to add subscriptions that can be called from main file.
+mqttClientSubscriptions = ["robot/toCmd_vel", "robot/intListToMqtt", "robot/boolToMqtt", "sensors/temperature", "sensors/humidity", "sensors/temperature2", "resistors/potmeter", "robot/fromCmd_vel"] #Make empty list here, and add function to add subscriptions that can be called from main file.
 
 # Function that creates an MQTT client, defines its callback functions and connects it to the broker via its IP address.
 def makeClient(clientName):
     client = mqtt.Client(client_id=clientName, protocol=mqtt.MQTTv311, transport="tcp")
     client.on_connect = on_connect
     client.on_message = on_message
-    client.message_callback_add("sensors/temperature", sensorsTemperatureCallback)
-    client.message_callback_add("sensors/temperature2", sensorsTemperature2Callback)
-    client.message_callback_add("sensors/humidity", sensorsHumidityCallback)
-    client.message_callback_add("resistors/potmeter", resistorsPotmeterCallback)
+    # client.message_callback_add("resistors/potmeter", resistorsPotmeterCallback)
     client.message_callback_add("robot/intListToMqtt", rosCallback)
     client.message_callback_add("robot/boolToMqtt", rosCallback)
     client.on_disconnect = on_disconnect
@@ -24,7 +25,9 @@ def makeClient(clientName):
 
 # Message callback function, to handle any messages on subscribed topics that don't have a specific callback function.
 def on_message(client, userdata, message):
-    print("Unrecognised Message Type Received: ", print(message.payload))
+    pass
+    #print("Received ROS Message: ", str(msgpack.unpackb(message.payload, raw=False)))
+    # print("Unrecognised Message Type Received: ", print(message.payload))
 
 # Callback function that executes when client receives CONNACK from broker in response to connect request.
 def on_connect(client, userdata, flags, rc):
@@ -36,31 +39,17 @@ def on_disconnect(client, userdata, rc):
     print("Disconnected: ", str(rc))
     client.loop_stop()
 
-# Message callback function, specifically to handle any messages on topics that match "sensors/temperature" (Including with wildcard use).
-def sensorsTemperatureCallback(client, userdata, message):
-    print("Received Temp Message: ", str(message.payload.decode("utf-8")))
-    #client.publish("actuators/servo", message.payload)  #Publishes messages received directly to actuators/servo topic
-
-# Message callback function, specifically to handle any messages on topics that match "sensors/temperature2" (Including with wildcard use).
-def sensorsTemperature2Callback(client, userdata, message):
-    print("Received Temp2 Message: ", str(message.payload.decode("utf-8"))) 
-
-# Message callback function, specifically to handle any messages on topics that match "sensors/humidity" (Including with wildcard use).
-def sensorsHumidityCallback(client, userdata, message):
-    print("Received Humidity Message: ", str(message.payload.decode("utf-8")))  
-
-# Message callback function, specifically to handle any messages on topics that match "sensors/potemeter" (Including with wildcard use).
-def resistorsPotmeterCallback(client, userdata, message):
-    data = json.loads(message.payload.decode("utf8"))
-    print("Received Potmeter Message: ", str(data))   
-    client.publish("actuators/servo", message.payload)  #Publishes messages received directly to actuators/servo topic
+# # Message callback function, specifically to handle any messages on topics that match "sensors/potemeter" (Including with wildcard use).
+# def resistorsPotmeterCallback(client, userdata, message):
+#     data = json.loads(message.payload.decode("utf8"))
+#     print("Received Potmeter Message: ", str(data))   
+#     client.publish("actuators/servo", message.payload)  #Publishes messages received directly to actuators/servo topic
 
 # Message callback function, specifically to handle any messages on topics related to ROS.
 def rosCallback(client, userdata, message):
     print("Received ROS Message: ", str(msgpack.unpackb(message.payload, raw=False)))   # Decodes MessagePack-encoded string into Python dictionary. 
                                                                                         # raw=False indicates decode string values as Unicode strings not binary 
                                                                                         # strings.
-
 # Publishes data to MQTT topics and distinguishes between normal topics and those for communication with the robot. 
 def mqttPublishData(client, topic, data):
         if type(data) is not dict:
